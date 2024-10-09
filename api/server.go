b1194b2da71a392090ba8ebc4803a542f5c3aab7
symyzi <89811910+symyzi@users.noreply.github.com>
2024-10-09 13:03:43 +0300

    login

--- api/server.go
+++ api/server.go
@@ -1,28 +1,44 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	db "github.com/symyzi/financial-helper/db/gen"
+	"github.com/symyzi/financial-helper/token"
+	"github.com/symyzi/financial-helper/util"
 )
 
 type Server struct {
-	store  *db.Store
-	router *gin.Engine
+	config     util.Config
+	store      *db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store *db.Store) *Server {
-	server := &Server{store: store}
+func NewServer(config util.Config, store *db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
 	router := gin.Default()
-
 	router.POST("/users", server.createUser)
-
+	router.POST("/users/login", server.loginUser)
 	router.POST("/wallets", server.createWallet)
 	router.GET("/wallets/:id", server.getWallet)
 	router.GET("/wallets", server.listWallets)
 	router.DELETE("/wallets/:id", server.deleteWallet)
-
 	server.router = router
-	return server
 }
 
 func (server *Server) Start(address string) error {
